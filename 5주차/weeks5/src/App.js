560d9a49622263ce751a5d826ce51604c7e03c11
TaeHyung <xoguddkenl@gmail.com>
2022-01-23 22:13:59 +0900

    test

--- 5주차/weeks5/src/App.js
+++ 5주차/weeks5/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import StyledComponent from './StyledComponent';
 
 function App() {
   return (
     <div>
-      test
+      <StyledComponent/>
     </div>
   );
 }
